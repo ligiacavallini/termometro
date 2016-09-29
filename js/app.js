8a1ed4be96bc78cb6ffb78ed0c59acbbebc27b9d
Ligia Cavallini Ribeiro <lcavalliniribeiro@watsco.com>
2016-09-29 10:24:21 -0400

    adding mountains and metter in the vertical center

--- js/app.js
+++ js/app.js
@@ -1,31 +1,33 @@
 $(window).on('resize',function() {
-    var height = $(window).height();
-    var half_height =  height / 2;
-    $('#weather').css("height", function(){
+    var height = $(window).height(),
+        half_height =  height / 2,
+        weather = $('#weather'),
+        amount,
+        percentage;
+
+    weather.css("height", function(){
         return height * 2;
     });
-    var amount  = (($(window).scrollTop() / height) * 100);
-    var percentage =  amount + "%";
-    $('.filler').css("height", amount);
-    $('.info').css("height", amount);
-    // scroll top = total height * 100
-    //(5 / 300) x 100
+    calc_temperature();
     $(window).scroll(function(){
+
+        calc_temperature();
+
+        if($(window).scrollTop() < half_height){
+            weather.addClass('winter');
+            weather.removeClass('summer');
+        }else{
+            weather.addClass('summer');
+            weather.removeClass('winter');
+        }
+    });
+
+    function calc_temperature(){
         amount = (($(window).scrollTop() / height) * 100);
         percentage =  amount + "%";
         $('.filler').css("height", percentage);
         $('.info').css("height", percentage);
         $('.number').text(parseInt(amount));
-
-        console.log(height);
-
-        if($(window).scrollTop() < half_height){
-            $('#weather').addClass('winter');
-            $('#weather').removeClass('summer');
-        }else{
-            $('#weather').addClass('summer');
-            $('#weather').removeClass('winter');
-        }
-    });
+    }
 }).trigger('resize');
 
