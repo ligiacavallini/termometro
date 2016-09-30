54f9aead99d4384529f2584beb4011f4176e97c8
Ligia Cavallini Ribeiro <lcavalliniribeiro@watsco.com>
2016-09-30 17:45:29 -0400

    Include prefixes to css using autoprefix. Include sun and clouds

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,23 @@
  * Created by ligiacavalliniribeiro on 9/28/16.
  */
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    postcss      = require('gulp-postcss'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function() {
     gulp.src('sass/style.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('library-styles', function() {
+    gulp.src('library/*/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('library/'));
 });
